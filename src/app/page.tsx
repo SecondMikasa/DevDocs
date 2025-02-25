0bd7904c1db99ccbb877c9f9cc46ab05f772b00d
Kumar Arnim <kumararnim1@vivaldi.net>
2025-02-26 01:18:26 +0530

    Initial setup, add required dependencies

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <p>
+        Hello World
+      </p>
+    </div>
+  )
+}
+
+// Page file in Next.js always has a export default 
+export default Home
